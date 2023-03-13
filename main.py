ded3d72ec3fe655c69a487a34b4fd8b67f31bdf4
aubin-tchoi <aubin.tchoi@gmail.com>
2023-03-13 22:51:35 +0100

    main script updated to play

--- main.py
+++ main.py
@@ -4,15 +4,21 @@ from deep_rl.environments.flappy_bird import FlappyBird
 from src import (
     infer_parameters,
     TreeBuilder,
-    predict_trajectory_success_rate,
+    get_best_action,
+    update_tree,
+    print_outcomes_stats,
+    checkpoint,
 )
 
 if __name__ == "__main__":
     params = {"gravity": 0.05, "force_push": 0.1, "vx": 0.05}
     env = FlappyBird(**params, prob_new_bar=1, max_height_bar=0.5)
 
+    timer = checkpoint()
+
     # parameters inference
     gravity, force_push, vx = infer_parameters(env)
+    timer("Time spent on parameters inference")
     assert np.allclose(
         [gravity, force_push, vx],
         (
@@ -20,21 +26,32 @@ if __name__ == "__main__":
             params["force_push"],
             params["vx"],
         ),
-        rtol=3,
+        rtol=1e-3,
     ), "Parameters inference failed"
 
     # tree building
     env.reset()
     (bird_x, bird_y, bird_vy), bars = env.step(0)[0]
     tree_builder = TreeBuilder(bars, gravity, force_push, vx)
-    tree = tree_builder.build_tree(bird_x, bird_y, bird_vy)
+    outcomes = tree_builder.build_tree(bird_x, bird_y, bird_vy)
     print(
-        f"Number of favorable outcomes: {tree.sum()} / {tree.shape[0]} ({tree.mean() * 100:.2f}%)"
+        f"\nNumber of leaves computed: {tree_builder.n_steps_computed}\n"
+        f"Number of leave computation steps saved: {tree_builder.n_steps_saved}\n"
     )
-    print("The two probabilities below are conditional to the current state.")
-    print(
-        f"Probability of losing when standing still: {predict_trajectory_success_rate(tree, 0):.2f}"
-    )
-    print(
-        f"Probability of losing when jumping: {predict_trajectory_success_rate(tree, 1):.2f}"
-    )
+    print_outcomes_stats(outcomes)
+
+    step, total_reward = 0, 0
+    while True:
+        action = get_best_action(outcomes)
+        observation, reward, done = env.step(action)
+        print(f"action: {action}, reward: {reward}, observation: {str(observation)}")
+
+        step += 1
+        total_reward += reward
+
+        print(f"Cumulated reward at step {step}: {total_reward:>3}.")
+        if done:
+            print(f"Simulation ended after {step} steps.")
+            break
+        outcomes = update_tree(action, observation, tree_builder, outcomes)
+        print_outcomes_stats(outcomes)
