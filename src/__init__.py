79034a979cb7335b076033dbeb8206a5c26c887c
aubin-tchoi <aubin.tchoi@gmail.com>
2023-03-13 22:51:35 +0100

    main script updated to play

--- src/__init__.py
+++ src/__init__.py
@@ -2,5 +2,9 @@ from .build_tree import (
     TreeBuilder,
     predict_trajectory_success_rate,
     predict_trajectory_outcome,
+    get_best_action,
+    update_tree,
+    print_outcomes_stats,
 )
 from .parameters_inference import infer_parameters
+from .perf_monitoring import checkpoint
