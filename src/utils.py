e99223215cd438450d3b2e5ee692d07b478df410
aubin-tchoi <aubin.tchoi@gmail.com>
2023-03-14 11:13:42 +0100

    added detailed trajectories printing

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
-import numpy as np
 from typing import List
+
+import numpy as np
 
 from .build_tree import predict_trajectory_success_rate
 from .typing import Observation
@@ -30,10 +31,10 @@ def print_outcomes_stats(outcomes: np.ndarray) -> None:
 
 
 def index_to_decisions(index: int, tree_depth: int) -> List[int]:
-    return [index // (2**i) for i in range(tree_depth)]
+    return [index // (2**i) % (2 ** (i + 1)) // 2**i for i in range(tree_depth)]
 
 
-def print_successful_trajectories(outcomes: np.ndarray, max_lines: int = 10) -> None:
+def print_successful_decisions(outcomes: np.ndarray, max_lines: int = 10) -> None:
     successful_trajectories = outcomes.nonzero()[0]
     indices = np.random.choice(
         successful_trajectories, min(max_lines, successful_trajectories.shape[0])
@@ -50,3 +51,33 @@ def print_successful_trajectories(outcomes: np.ndarray, max_lines: int = 10) -> 
             for index in indices
         )
     )
+
+
+def print_successful_trajectories(
+    bird_x: float,
+    bird_y: float,
+    bird_vy: float,
+    gravity: float,
+    force_push: float,
+    vx: float,
+    outcomes: np.ndarray,
+    max_lines: int = 3,
+) -> None:
+    successful_trajectories = outcomes.nonzero()[0]
+    indices = np.random.choice(
+        successful_trajectories, min(max_lines, successful_trajectories.shape[0])
+    )
+    print("\nA few successful trajectories:")
+    for index in indices:
+        new_bird_x, new_bird_y, new_bird_vy = bird_x, bird_y, bird_vy
+        print(f" -- ({bird_x:.2f}, {bird_y:.2f}, {bird_vy:.2f})", end="")
+        decisions = index_to_decisions(index, int(np.log2(outcomes.shape[0])))
+        for decision in decisions:
+            new_bird_x += vx
+            new_bird_vy += -gravity + force_push * decision
+            new_bird_y += new_bird_vy
+            print(
+                f" -> {'jumps' if decision else 'falls'} to ({new_bird_x:.2f}, {new_bird_y:.2f}, {new_bird_vy:.2f})",
+                end="",
+            )
+        print()
